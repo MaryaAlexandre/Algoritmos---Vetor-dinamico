72d726fb512502c7ccf08acfe5fa1ecee935623c
Marya Eduarda Alexandre <147219061+MaryaAlexandre@users.noreply.github.com>
2024-07-24 19:42:21 -0300

    Add files via upload

---
+++ testeremover_em.cpp
@@ -0,0 +1,24 @@
+#include "vetor.hpp"
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  vetor lista;
+  for (int i = 0; i < 10000; i++) {
+    lista.inserir(i);
+  }
+
+  auto inc = chrono::high_resolution_clock::now();
+
+  while (cin >> n) {
+    lista.remover_em(n);
+  }
+
+  auto fim = chrono::high_resolution_clock::now();
+  auto duracao = fim - inc;
+
+  cout << "TEMPO TOTAL: " << duracao.count() << endl;
+}
